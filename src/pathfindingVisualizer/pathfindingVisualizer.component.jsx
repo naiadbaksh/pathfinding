e392c964cb2203f0b7d0df8fec1ed0e7904a5fab
Naiad Baksh <naiadbaksh@gmail.com>
2021-09-03 20:18:59 -0400

    - creating folder structure and files

---
+++ src/pathfindingVisualizer/pathfindingVisualizer.component.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./pathfindingVisualizer.styles.css";
+import Node from "./Node/node.component";
+import ToolBar from "./toolbar.component";
+
+const PathfindingVisualizer = (props) => {
+  return <div>This is a Pathfinder.</div>;
+};
+
+export default PathfindingVisualizer;
